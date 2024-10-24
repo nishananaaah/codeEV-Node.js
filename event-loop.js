4a425e9bb9eaae395e382fa0280d8b82b6cc9b75
nishananaaah <nishanaf731@gmail.com>
2024-10-24 16:09:13 +0530

    event-loop

---
+++ event-loop.js
@@ -0,0 +1,29 @@
+//Micro-Task-Queues
+// console.log("console.1");
+// process.nextTick(()=>console.log("this process.next 1"))//nextTick queue
+// console.log("console.2")
+
+// Promise.resolve().then(()=>console.log("this is promise.resolve 1"))//promise queue //after
+// process.nextTick(()=>console.log("this is process.next 1"))//before
+
+
+//Timer queues
+setTimeout(()=>console.log("this is setTimeout 1"),0)
+setTimeout(()=>console.log("this is setTimeout 2"),0)
+setTimeout(()=>console.log("this is setTimeout 3"),0)
+process.nextTick(()=>console.log("this is process.next 1"));
+process.nextTick(()=>{
+    console.log("this is process.next 2");
+    process.nextTick(()=>
+    console.log("this is the inner tick inside the tick"))
+});
+process.nextTick(()=>console.log("this is process.next 3"));
+
+Promise.resolve().then(()=>console.log("this is promise.resolve 1"));
+Promise.resolve().then(()=>{
+    console.log("this is promise.resolve 2");
+    process.nextTick(()=>
+      console.log("this is the inner next tick inside the promise block")//nextTick queue is the prioty
+    );
+});
+Promise.resolve().then(()=> console.log("this is promise.resolve 3"))
--- threadpool.js
+++ threadpool.js
@@ -4,7 +4,7 @@
 //   console.log("file contents")
 // });
 // console.log("last")
-const cryptpo = require("node:crypto");
+// const cryptpo = require("node:crypto");
 //synchrous
 // cryptpo.pbkdf2 //password base key derivation function 2 = to hash password before storing them
 //measuring the time of code executing in synchronus
@@ -13,10 +13,27 @@ const cryptpo = require("node:crypto");
 // cryptpo.pbkdf2Sync("password","salt",100000,512,"sha512");//indicating synchronus and blocking
 // console.log("Hash:",Date.now()-start);
 //asynchronus
-const MAX_CALLS =3;
-const start = Date.now();
-for(let i=0;i<MAX_CALLS;i++){
-    cryptpo.pbkdf2("password","salt",100000,512,"sha512",()=>{//runs in a seperate thread
-        console.log(`Hash:${i+1}`,Date.now()-start)
+// process.env.UV_THREADPOOL_SIZE =16;//8 cpu cores
+// const MAX_CALLS =16;
+// const start = Date.now();
+// for(let i=0;i<MAX_CALLS;i++){
+//     cryptpo.pbkdf2("password","salt",100000,512,"sha512",()=>{//runs in a seperate thread
+//         console.log(`Hash:${i+1}`,Date.now()-start)//Libuv thread pool has 4 threads
+//     })
+// }
+
+//Network I/O
+const https =require("node:https");
+
+const MAX_CALLS =12;
+const start =Date.now();
+for(let i=0; i < MAX_CALLS; i++){
+https
+.request("https://www.google.com",(res)=>{
+    res.on("data",()=>{});
+    res.on("end",()=>{
+        console.log(`Request:${i+1}`,Date.now()-start);
     })
+  })
+  .end();
 }
